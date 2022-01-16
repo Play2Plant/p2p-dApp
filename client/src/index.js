52b2d087a31f4bb63d1e6f3b80ce0b64b1352c81
Boyquotes <webmaster@linux-live-cd.org>
2022-01-16 20:35:10 +0100

    Load NFT page first

--- client/src/index.js
+++ client/src/index.js
@@ -16,7 +16,7 @@ ReactDOM.render(
        <Router>
       <Header/>
       <Switch>
-        <Route exact path="/" component={App} />
+        <Route exact path="/" component={Marketplace} />
         <Route path="/service" component={Service} />
         <Route path="/about" component={About} />
         <Route path="/nft" component={Marketplace} />

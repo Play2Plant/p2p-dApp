54d28d6e77edaef789c607f621667ac85de701db
Slymaster <sylvain.rey.pro@gmail.com>
2022-01-19 18:09:08 +0100

    remove unused App var

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from './components/Header'

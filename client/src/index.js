401ef12677ff6ed54f9cedfbf0c7c3f43ef0e8ed
Boyquotes <webmaster@linux-live-cd.org>
2022-01-19 18:57:18 +0100

    Fix App missing for homepage

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from './components/Header'
